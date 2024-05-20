07fe9563b9b8eb0cadee4853ac634aa9e6c8259d
iannickgagnon <iannick.gagnon@etsmtl.ca>
2024-05-20 07:23:16 -0400

    Added Sphere image

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from src.objective_functions.subclasses.levy import Levy
 
 if __name__ == "__main__":
 
-    foo = Rosenbrock().visualize()
+    foo = Sphere().visualize()
 
 
     #foo.apply_noise(0, 0.5)
